d2d83eeaf7ebd65c2c0cf5fd5aba83e654c31f71
Shohrab Rustam <mohdrustam001@gmail.com>
2022-03-02 22:33:33 +0530

    set the master

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
 
 /*
@@ -19,6 +18,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
