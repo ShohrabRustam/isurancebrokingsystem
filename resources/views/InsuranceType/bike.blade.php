611197d664f7436a9a353716737f1c5a079d3311
Shohrab Rustam <mohdrustam001@gmail.com>
2022-03-03 09:35:04 +0530

    set the page of the insurance type

---
+++ resources/views/InsuranceType/bike.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title')
+Insurance
+@endsection
+
+@section('section')
+<h2>Bike Insurance Page</h2>
+
+@endsection
+
---
+++ resources/views/InsuranceType/car.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title')
+Insurance
+@endsection
+
+@section('section')
+<h2>Car Insurance Page</h2>
+
+@endsection
+
