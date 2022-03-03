377b0b46f918b24a6e5e447e9abfe9ad6b0219f1
Shohrab Rustam <mohdrustam001@gmail.com>
2022-03-03 09:28:46 +0530

    add the pages

--- routes/web.php
+++ routes/web.php
@@ -20,27 +20,23 @@ Route::get('/', function () {
     return view('home');
 });
 
-// Route::get('about', function () {
-//     return view('about');
-// });
-// Route::get('contact', function () {
-//     return view('contact');
-// });
-// Route::get('HealthInsurance', function () {
-//     return view('InsuranceType.health');
-// });
-// Route::get('BikeInsurance', function () {
-//     return view('InsuranceType.bike');
-// });
-// Route::get('CarInsurance', function () {
-//     return view('InsuranceType.car');
-// });
+Route::get('about', function () {
+    return view('about');
+});
+Route::get('contact', function () {
+    return view('contact');
+});
+Route::get('HealthInsurance', function () {
+    return view('InsuranceType.health');
+});
+Route::get('BikeInsurance', function () {
+    return view('InsuranceType.bike');
+});
+Route::get('CarInsurance', function () {
+    return view('InsuranceType.car');
+});
 
-// Route::get('login', function () {
-//     return view('login');
-// });
-
-// Route::get('registration', function () {
-//     return view('registration');
-// });
+Route::get('login', function () {
+    return view('login');
+});
 
