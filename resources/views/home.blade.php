770d1d0a27de42f5b085e49eef7f7799b0038794
Shohrab Rustam <mohdrustam001@gmail.com>
2022-03-06 13:39:08 +0530

    add the style to the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,16 +7,13 @@ Home
 
 @section('section')
 
-
-<h1>Home</h1>
-
 <div class="row row-cols-1 row-cols-md-4 g-4 align-self-center" >
     <div class="col" style="width: auto; margin:3rem; ">
-        <div class="card" style="width: 20rem;">
+        <div class="card" style="width: 20rem; background-color:rgb(225, 224, 233); border-radius:20px">
             <a href="LifeInsurance" >
-            <img src="https://life.futuregenerali.in/media/408244/traditional-life-insurance.jpg" class="card-img-top" alt="..." style="    max-width:20rem;    max-height:10rem; min-width:20rem; min-height:10rem;">
+            <img src="https://img.etimg.com/thumb/msid-81508498,width-640,resizemode-4,imgsize-106253/why-the-increase.jpg" class="card-img-top" alt="..." style="    max-width:20rem;    max-height:10rem; min-width:20rem; min-height:10rem; border-radius:20px; background-color:rgb(9, 15, 1);">
             <div class="card-body">
-              <h5 class="card-title">Term Life Insurance</h5>
+              <h5 class="card-title">Life Insurance</h5>
               {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
             </div>
         </a>
@@ -24,9 +21,9 @@ Home
     </div>
 
     <div class="col" style="width: auto; margin:3rem;">
-        <div class="card" style="width: 20rem;">
+        <div class="card" style="width: 20rem; background-color:rgb(225, 224, 233); border-radius:20px">
             <a href="HealthInsurance" >
-            <img src="https://www.icicilombard.com/docs/default-source/assets/freshlook/images/ui-health-insurance-policy.png" class="card-img-top" alt="..." style="    max-width:20rem;    max-height:10rem; min-width:20rem; min-height:10rem; ">
+            <img src="https://www.icicilombard.com/docs/default-source/assets/freshlook/images/ui-health-insurance-policy.png" class="card-img-top" alt="..." style="    max-width:20rem;    max-height:10rem; min-width:20rem; min-height:10rem; border-radius:20px; background-color:rgb(9, 15, 1); ">
             <div class="card-body">
               <h5 class="card-title">Health Insurance</h5>
               {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
@@ -37,9 +34,9 @@ Home
     </div>
 
     <div class="col" style="width: auto; margin:3rem; ">
-        <div class="card"  style="width: 20rem;">
+        <div class="card"  style="width: 20rem; background-color:rgb(225, 224, 233); border-radius:20px">
             <a href="BikeInsurance" >
-            <img src="https://images.ctfassets.net/uwf0n1j71a7j/3hsbWvbJauXM1g4VTLDn6A/c0bdbe4ea5cf6b57e0547db4e268d95b/motor-insurance-policy-online.png" class="card-img-top" alt="..." style="    max-width:20rem;    max-height:10rem; min-width:20rem; min-height:10rem;  ">
+            <img src="https://images.ctfassets.net/uwf0n1j71a7j/3hsbWvbJauXM1g4VTLDn6A/c0bdbe4ea5cf6b57e0547db4e268d95b/motor-insurance-policy-online.png" class="card-img-top" alt="..." style="    max-width:20rem;    max-height:10rem; min-width:20rem; min-height:10rem; border-radius:20px; background-color:rgb(9, 15, 1); ">
             <div class="card-body">
               <h5 class="card-title">Car Insurance</h5>
               {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
@@ -50,9 +47,9 @@ Home
     </div>
 
     <div class="col"  style="width: auto; margin:3rem; ">
-        <div class="card" style="width: 20rem;">
+        <div class="card" style="width: 20rem;background-color:rgb(225, 224, 233); border-radius:20px ">
             <a href="CarInsurance" >
-            <img src="https://www.bharti-axagi.co.in/drupal_backend/sites/default/files/2019-07/vector-smart-object%402x.png" class="card-img-top" alt="..." style="max-width:20rem; max-height:10rem; min-width:20rem; min-height:10rem; ">
+            <img src="https://www.bharti-axagi.co.in/drupal_backend/sites/default/files/2019-07/vector-smart-object%402x.png" class="card-img-top" alt="..." style="max-width:20rem; max-height:10rem; min-width:20rem; min-height:10rem;  border-radius:20px; background-color:rgb(9, 15, 1);">
             <div class="card-body">
               <h5 class="card-title">Bike Insurance</h5>
               {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
