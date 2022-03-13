d80b0f7f956f253e0b65defd46b4f57a952c55f0
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-14 00:34:47 +0530

    facker

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,8 +7,12 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use Faker\Factory as Faker;
+
+// user_registration models
 use App\Models\user_registration;
+//facker
+use Faker\Factory as Faker;
+
 
 class UserSeeder extends Seeder
 {
@@ -19,25 +23,31 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = new user_registration();
-        $faker = FaKer::create();
-        // DB::table('user_registration')->insert([
+        // $user = new user_registration();
+        // $faker = FaKer::create();
+        // DB::table('user_registration')->insert
 
-            // 'name' => 'Str::random(10)',
-            // 'email' => Str::random(10).'@gmail.com',
-            // 'password' => Hash::make('password')
-                // 'name' => 'Rustam',
-                // 'email' => 'rustam001@gmail.com',
-            //    'password' => Hash::make('password')
-            //    'password' =>'password'
+        // 'name' => 'Str::random(10)',
+        // 'email' => Str::random(10).'@gmail.com',
+        // 'password' => Hash::make('password')
+        // 'name' => 'Rustam',
+        // 'email' => 'rustam001@gmail.com',
+        //    'password' => Hash::make('password')
+        //    'password' =>'password'
 
-            $user->name = "Shohab Rustam";
-            $user->email = "mohdrustam001@gmail.com";
-            $user->password = "password";
-            $user->save();
+        // $user->name = "Shohab Rustam";
+        // $user->email = "mohdrustam001@gmail.com";
+        // $user->password = "password";
+        // $user->save();
 
-
-        // ]);
-
+        // for ($i = 0; $i < 100; $i++)
+        // {
+        //     $user->name = $faker->name;
+        //     // for unique email
+        //     $user->email = $faker->unique()->email;
+        //     $user->password = "password";
+        //     $user->save();
+        // }
+            user_registration::factory(100)->create();
     }
 }
